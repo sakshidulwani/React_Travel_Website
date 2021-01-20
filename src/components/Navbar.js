d1e502df995c58fb6e914bdad41229702b69fadd
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-21 00:10:06 +0530

    changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+
+            </div>
+        </nav>
+    );
+}
+
+
+export default Navbar;
