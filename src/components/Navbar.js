6e47d9949be32eaa4040750c9d53ef2bd6d686d2
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-28 19:48:24 +0530

    Changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { Button } from './Button';
+import '../Css/Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -15,7 +16,13 @@ function Navbar() {
         } else {
             setButton(true);
         }
-    }
+    };
+
+    useEffect(() => {
+        showButton()
+    }, []);
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
